c0b562323a16b36f259a5fc8c9494bb8acd19da9
Matthias Brun <matthias.brun@gmail.com>
2021-09-19 12:38:08 +0200

    Initial commit

---
+++ src/main/java/ch/dsi/cas/cognito/HomeController.java
@@ -0,0 +1,14 @@
+package ch.dsi.cas.cognito;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
